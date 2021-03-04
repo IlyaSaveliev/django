005ca2af49bee809b3c77d33c8a57853fa8d9fd1
Saveliev.I <parazit.ololo@gmail.com>
2021-03-04 13:17:14 +0300

    Changed for HW_2

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
-# Create your views here.
+def main(request):
+    content = {
+        'title': 'Главная'
+    }
+    return render(request, 'mainapp/index.html', content)
+
+def products(request):
+    links_menu = [
+        {'href': 'product_all', 'name': 'все'},
+        {'href': 'product_home', 'name': 'дом'},
+        {'href': 'product_office', 'name': 'офис'},
+        {'href': 'product_modern', 'name': 'модерн'},
+        {'href': 'product_classic', 'name': 'классика'},
+    ]
+    content = {
+        'title': 'Продукты',
+        'links_menu': links_menu,
+    }
+    return render(request, 'mainapp/products.html', content)
+
+def contact(request):
+    content = {
+        'title': 'Контакты'
+    }
+    return render(request, 'mainapp/contact.html', content)
