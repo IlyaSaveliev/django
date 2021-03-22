7a6614a20745a83a5a5778d3c8220fccaeed0bff
Saveliev.I <parazit.ololo@gmail.com>
2021-03-22 12:54:50 +0300

    modified for HW7

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -13,7 +13,9 @@ urlpatterns = [
     path('contact/', mainapp.contact, name='contact'),
     path('auth/', include('authapp.urls', namespace='auth')),
 
-    path('admin/', admin.site.urls),
+    path('admin/', include('adminapp.urls', namespace='admin')),
+
+    # path('admin/', admin.site.urls),
 ]
 
 if settings.DEBUG:
